0d14ed79548e725fef61cd5291773917e6dd490b
gschleusner <greg.schleusner@Hok.com>
2018-07-12 11:26:33 -0400

    First
    First

---
+++ data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main-road-east",
+      "name": "Main Road East",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.05104515625289707,
+        "pitch": -0.01954610813886859,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.426283131808514,
+          "pitch": 0.24425811130556596,
+          "rotation": 6.283185307179586,
+          "target": "2-courtyard-west-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-main-road-center-",
+      "name": "Main Road Center ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.8587720231894433,
+        "pitch": -0.017535791836909453,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-courtyard-west-",
+      "name": "Courtyard West ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.0187767493797057,
+        "pitch": 0.05564011595595275,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11511361799393072,
+          "pitch": 0.36004680139918577,
+          "rotation": 0,
+          "target": "3-courtyard-center---"
+        },
+        {
+          "yaw": 0.10664937305697819,
+          "pitch": 0.1456326382953197,
+          "rotation": 0,
+          "target": "4-courtyard-east"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-courtyard-center---",
+      "name": "Courtyard Center   ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 2.6631435177961738,
+        "pitch": -0.0573349340116458,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.017401785520343438,
+          "pitch": 0.27843252614969316,
+          "rotation": 0,
+          "target": "4-courtyard-east"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-courtyard-east",
+      "name": "Courtyard East",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.3914283777215939,
+        "pitch": 0.20677518018649366,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9369647991699352,
+          "pitch": 0.5145036148449851,
+          "rotation": 0,
+          "target": "6-podium-west--"
+        },
+        {
+          "yaw": -0.019077720496214923,
+          "pitch": 0.4691914972225817,
+          "rotation": 0,
+          "target": "3-courtyard-center---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-podium-east-",
+      "name": "Podium East ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.7774947228716904,
+        "pitch": 0.0017376065561887089,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22450100163031905,
+          "pitch": 0.43726729177630297,
+          "rotation": 0,
+          "target": "6-podium-west--"
+        },
+        {
+          "yaw": 1.8057773224643752,
+          "pitch": 0.6463701702280211,
+          "rotation": 0,
+          "target": "3-courtyard-center---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-podium-west--",
+      "name": "Podium West  ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 3.139236995673728,
+        "pitch": 0.18287579885253535,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.101201844578359,
+          "pitch": 0.3954792905128883,
+          "rotation": 0,
+          "target": "5-podium-east-"
+        },
+        {
+          "yaw": 0.7985518564334821,
+          "pitch": 0.5061797573187672,
+          "rotation": 0,
+          "target": "4-courtyard-east"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "18.07027",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
